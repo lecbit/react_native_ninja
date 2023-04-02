078ed87f84f78bacdd18d3d9f3a8bae582484b99
lecbit <artm-marych@rambler.ru>
2023-04-02 01:58:58 +0100

    ~10~ Todo 2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,13 @@ export default function App() {
     { text: 'Bcd bbbbbbbbb', key: 2 },
     { text: 'Cdf cccccccccc', key: 3 },
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((oldTodos) => {
+      return oldTodos.filter(todo => todo.key != key)
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header />
@@ -16,7 +24,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
           />
         </View>
